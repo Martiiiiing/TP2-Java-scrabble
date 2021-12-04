a7adc125284820e540126f614916d549f9195f98
Martin <47635335+Martiiiiing@users.noreply.github.com>
2021-12-04 18:18:18 +0100

    first commit

---
+++ Dictionary.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Dictionary {
+    //this class gets the words from the file and stores them in a hashmap
+    private String[] wordList;
+
+    
+    public void Dictionary(String fileName) {
+        //this method reads the file and stores the words in the hashmap
+        try {
+            java.util.Scanner file = new java.util.Scanner(new java.io.File(fileName));
+            wordList=new String[file.nextInt()];
+            for(int i=0;i<wordList.length;i++){ 
+                wordList[i]=file.nextLine();  
+            }
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        sortWords();
+    }
+    public boolean isValidWord(String word) {
+        //this method checks if the word is in the hashmap
+        for(int i=0;i<wordList.length;i++){
+            if(wordList[i].equals(word)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private void sortWords() {
+        //this method sorts the words in the hashmap
+        java.util.Arrays.sort(wordList);
+    }
+}
---
+++ ScrabbleConsole.java
@@ -0,0 +1,14 @@
+public class ScrabbleConsole {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ScrabbleConsole scrabble = new ScrabbleConsole();
+        System.out.println("Welcome to the Scrabble assistant");
+    }
+
+    //constructor for ScrabbleConsole
+    public ScrabbleConsole() {
+        
+    }
+
+    
+}
