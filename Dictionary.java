1a1d9e02b7062594dcf387b8663abda57e8469dd
Martin <47635335+Martiiiiing@users.noreply.github.com>
2021-12-09 14:20:14 +0100

    mayBeComposedFunction+BinarySearch+Main

--- Dictionary.java
+++ Dictionary.java
@@ -1,34 +1,106 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Dictionary {
     //this class gets the words from the file and stores them in a hashmap
     private String[] wordList;
-
+    private String fileName;
+    public int size;
     
-    public void Dictionary(String fileName) {
-        //this method reads the file and stores the words in the hashmap
+    public Dictionary(String FileName) {
+        this.fileName = FileName;
+
         try {
             Scanner file = new Scanner(new java.io.File(fileName));
-            wordList=new String[file.nextInt()];
+            size = file.nextInt();
+            wordList=new String[size];
             for(int i=0;i<wordList.length;i++){ 
                 wordList[i]=file.nextLine();  
             }
         } catch (java.io.FileNotFoundException e) {
             System.out.println("File not found");
         }
+
         sortWords();
     }
     public boolean isValidWord(String word) {
         //this method checks if the word is in the hashmap
-        for(int i=0;i<wordList.length;i++){
-            if(wordList[i].equals(word)){
-                return true;
-            }
+        // for(int i=0;i<wordList.length;i++){
+        //     if(wordList[i].equals(word)){
+        //         return true;
+        //     }
+        // }
+        if(Arrays.binarySearch(wordList,word)>=0){
+            return true;
         }
         return false;
+
+        
     }
     private void sortWords() {
         //this method sorts the words in the hashmap
         java.util.Arrays.sort(wordList);
+    }
+    public boolean mayBeComposed(String word, char[] letters) {
+        boolean[] isUsed = new boolean[letters.length]; // isUsed is of the same size as letters
+        Arrays.fill(isUsed, false); // set  all  elements  to  false
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = -1;
+            for (int j = 0; j < letters.length; j++) {
+                if (letters[j] == c && !isUsed[j]) {
+                    index = j;
+                    break;
+                }
+            }
+            if (index == -1) {
+                return false;
+            }
+            isUsed[index] = true;
+        }
+        return true;
+        // //this method checks if the word can be composed from the letters
+        // // if(word.length()==1){
+        // //     if(letters.length==1){
+        // //         if(word.charAt(0)==letters[0]){
+        // //             return true;
+        // //         }
+        // //     }
+        // // }
+        // // else{
+        // //     for(int i=0;i<word.length();i++){
+        // //         if(letters.length==1){
+        // //             if(word.charAt(i)==letters[0]){
+        // //                 return true;
+        // //             }
+        // //         }
+        // //         else{
+        // //             for(int j=0;j<letters.length;j++){
+        // //                 if(word.charAt(i)==letters[j]){
+        // //                     letters[j]=' ';
+        // //                     break;
+        // //                 }
+        // //             }
+        // //         }
+        // //     }
+        // // }
+        // // return false;
+        // for(int i=0;i<word.length();i++){
+        //     if(letters.length==1){
+        //         if(word.charAt(i)==letters[0]){
+        //             return true;
+        //         }
+        //     }
+        //     else{
+        //         for(int j=0;j<letters.length;j++){
+        //             if(word.charAt(i)==letters[j]){
+        //                 letters[j]=' ';
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        // return false;
+
     }
 }
--- ScrabbleConsole.java
+++ ScrabbleConsole.java
@@ -1,13 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ScrabbleConsole {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        ScrabbleConsole scrabble = new ScrabbleConsole();
         System.out.println("Welcome to the Scrabble assistant");
-    }
-
-    //constructor for ScrabbleConsole
-    public ScrabbleConsole() {
-        
+        Dictionary dictionary = new Dictionary("fr_FR_utf8.dico");
+        System.out.println(dictionary.size+ " words loaded");
+        System.out.println("Please enter a word:");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(dictionary.isValidWord(word));
+        System.out.println("Please enter a letter list:");
+        char[] letters = new char[7];
+        for (int i = 0; i < 7; i++) {
+            letters[i] = sc.next().charAt(0);
+        }
+        System.out.println(letters);
+        if (dictionary.mayBeComposed(word, letters)) {
+            System.out.println(word+ " may be composed with letters: "+ Arrays.toString(letters));
+        } else {
+            System.out.println(word+ " may be composed with letters: "+ Arrays.toString(letters));
+        }
     }
 
     
